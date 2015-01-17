c2486d53f1af0ea709714a56bf7743232155571f
Adam Klein <asklein@gmail.com>
2015-01-17 15:36:17 -0800

    deleted cordova dir and reinitialized for ios

--- config/environment.js
+++ config/environment.js
@@ -1,85 +1,52 @@
 /* jshint node: true */
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'ember-cordova-test',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'hash',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
 
-var addresses = [];
-for (var dev in ifaces) {
-    ifaces[dev].forEach(function (details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-            addresses.push(details.address);
-        }
-    });
-}
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
 
-
-module.exports = function (environment) {
-    var platform = 'android';
-    if (environment === 'ios') {
-        platform = 'ios';
+    cordova: {
+      rebuildOnChange: true,
+      emulate: true
     }
-    var ENV = {
-        modulePrefix: 'ember-cordova-test',
-        environment: environment,
-        baseURL: '/',
-        locationType: 'hash',
-        EmberENV: {
-            FEATURES: {
-                // Here you can enable experimental features on an ember canary build
-                // e.g. 'with-controller': true
-            }
-        },
+  };
 
-        APP: {
-            // Here you can pass flags/options to your application instance
-            // when it is created
-        },
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
 
-        cordova: {
-            rebuildOnChange: true,
-            emulate: true,
-            platform: platform,
-            emberUrl: 'http://' + addresses[0] + ':4200',
-            liveReload: {
-                enabled: true,
-                platform: platform
-            }
-        }
-    };
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'auto';
 
-    if ((environment === 'development') || (environment === 'ios')) {
-        // ENV.APP.LOG_RESOLVER = true;
-        ENV.APP.LOG_ACTIVE_GENERATION = true;
-        // ENV.APP.LOG_TRANSITIONS = true;
-        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-        ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-        ENV.apiUrl = 'http://' + addresses[0] + ':3000/api/v1';
-        ENV.development = true;
-    }
+    ENV.APP.rootElement = '#ember-testing';
+  }
 
-    if (environment === 'test') {
-        // Testem prefers this...
-        ENV.baseURL = '/';
-        ENV.locationType = 'auto';
+  if (environment === 'production') {
 
-        // keep test console output quieter
-        ENV.APP.LOG_ACTIVE_GENERATION = false;
-        ENV.APP.LOG_VIEW_LOOKUPS = false;
+  }
 
-        ENV.APP.rootElement = '#ember-testing';
-    }
-
-    if (environment === 'staging') {
-        ENV.apiUrl = 'http://ember-cordova-test-staging.herokuapp.com/api/v1';
-        ENV.staging = true;
-    }
-
-
-    if (environment === 'production') {
-        ENV.apiUrl = 'http://ember-cordova-test.herokuapp.com/api/v1';
-        ENV.production = true;
-    }
-
-    return ENV;
+  return ENV;
 };
