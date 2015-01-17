56b36539b7d94aaad832d00c44ca56270d302ebb
aklein <asklein@gmail.com>
2015-01-16 23:58:18 -0800

    1.  making updates before adding ios

--- config/environment.js
+++ config/environment.js
@@ -1,80 +1,85 @@
 /* jshint node: true */
 
-var os     = require('os');
+var os = require('os');
 var ifaces = os.networkInterfaces();
 
 var addresses = [];
 for (var dev in ifaces) {
-  ifaces[dev].forEach(function(details){
-    if(details.family === 'IPv4' && details.address !== '127.0.0.1') {
-      addresses.push(details.address);
-    }
-  });
+    ifaces[dev].forEach(function (details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+            addresses.push(details.address);
+        }
+    });
 }
 
-module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'ember-cordova-test',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'hash',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
-
-    cordova: {
-      rebuildOnChange: true,
-      emulate: true,
-      platform: 'android',
-      emberUrl: 'http://' + addresses[0] + ':4200',
-      liveReload: {
-        enabled: true,
-        platform: 'android'
-      }
+module.exports = function (environment) {
+    var platform = 'android';
+    if (environment === 'ios') {
+        platform = 'ios';
     }
-  };
+    var ENV = {
+        modulePrefix: 'ember-cordova-test',
+        environment: environment,
+        baseURL: '/',
+        locationType: 'hash',
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
+            }
+        },
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+        APP: {
+            // Here you can pass flags/options to your application instance
+            // when it is created
+        },
 
-    ENV.apiUrl   = 'http://' + addresses[0] + ':3000/api/v1';
-    ENV.development = true;
-  }
+        cordova: {
+            rebuildOnChange: true,
+            emulate: true,
+            platform: platform,
+            emberUrl: 'http://' + addresses[0] + ':4200',
+            liveReload: {
+                enabled: true,
+                platform: platform
+            }
+        }
+    };
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+    if ((environment === 'development') || (environment === 'ios')) {
+        // ENV.APP.LOG_RESOLVER = true;
+        ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+        ENV.apiUrl = 'http://' + addresses[0] + ':3000/api/v1';
+        ENV.development = true;
+    }
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.baseURL = '/';
+        ENV.locationType = 'auto';
 
-  if (environment === 'staging') {
-    ENV.apiUrl = 'http://ember-cordova-test-staging.herokuapp.com/api/v1';
-    ENV.staging = true;
-  }
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+        ENV.APP.rootElement = '#ember-testing';
+    }
+
+    if (environment === 'staging') {
+        ENV.apiUrl = 'http://ember-cordova-test-staging.herokuapp.com/api/v1';
+        ENV.staging = true;
+    }
 
 
-  if (environment === 'production') {
-    ENV.apiUrl = 'http://ember-cordova-test.herokuapp.com/api/v1';
-    ENV.production = true;
-  }
+    if (environment === 'production') {
+        ENV.apiUrl = 'http://ember-cordova-test.herokuapp.com/api/v1';
+        ENV.production = true;
+    }
 
-  return ENV;
+    return ENV;
 };
